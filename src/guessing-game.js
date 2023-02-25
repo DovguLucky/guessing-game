e855ce3a9f6e716024c459170dc83d8bb33caa04
DovguLucky <dovgluch98@gmail.com>
2023-02-25 12:45:11 +0200

    add:good job!

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,19 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return Math.ceil((this.max - this.min) / 2) + this.min;
     }
 
     lower() {
+        this.max = this.guess()
 
     }
 
     greater() {
+        this.min = this.guess()
 
     }
 }
